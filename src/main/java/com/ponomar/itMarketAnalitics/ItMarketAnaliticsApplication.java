714499670d45ddf02f6f15d282803c8bc73830d6
Ponomar <artemponomar98@gmail.com>
2019-05-10 19:56:38 +0300

    initial

---
+++ src/main/java/com/ponomar/itMarketAnalitics/ItMarketAnaliticsApplication.java
@@ -0,0 +1,13 @@
+package com.ponomar.itMarketAnalitics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItMarketAnaliticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItMarketAnaliticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ponomar/itMarketAnalitics/ItMarketAnaliticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ponomar.itMarketAnalitics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItMarketAnaliticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
